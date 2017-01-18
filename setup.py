00200030bc03dc27c86798c105f6e3d40386e13b
Adam Wentz <adam.wentz@dollarshaveclub.com>
2017-01-17 22:45:26 -0800

    bump

--- setup.py
+++ setup.py
@@ -4,17 +4,19 @@ from distutils.core import setup
 
 setup(
     name='gym-qbrush',
-    version='0.0.1',
+    version='0.0.2',
     description='AI Gym environment for drawing images',
     author='Adam Wentz',
     author_email='adam@adamwentz.com',
     url='https://github.com/awentzonline/gym-qbrush',
     packages=[
         'gym_qbrush',
+        'gym_qbrush.environments',
     ],
     install_requires=[
         'gym',
         'Pillow',
-        'numpy'
+        'numpy',
+        'keras'
     ]
 )
